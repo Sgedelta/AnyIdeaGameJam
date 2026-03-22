9f37c3190290f39eb417635035f95c6d9c3c715d
FMPO <ple5024@rit.edu>
2026-03-22 16:07:18 -0400

    debug thrusters

--- Scenes/AsteroidField.cs
+++ Scenes/AsteroidField.cs
@@ -5,7 +5,7 @@ using System;
 public partial class AsteroidField : Node3D
 {
 	[Export]
-	public float shipSpeed = 3f;
+	public float shipSpeed = 20f;
 	public Vector3 shipVelocity = Vector3.Zero;
 
 	private Area3D goalCollision;
@@ -17,13 +17,15 @@ public partial class AsteroidField : Node3D
 
     public bool rightThrust = false;
 	public bool leftThrust = false;
+	public int forwardThrust = 0;
+    int thrust1 = 0;
+    int thrust2 = 0;
 
-	public Array<Area3D> asteroidColliders = new Array<Area3D>();
+    public Array<Area3D> asteroidColliders = new Array<Area3D>();
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		shipVelocity = new Vector3(0, 0, 3f);
         shipCollider = GetTree().GetFirstNodeInGroup("ship").GetNode<Area3D>("Area3D");
 		//if (shipCollider != null) GD.Print("Shippa!");
         goalCollision = GetNode<Area3D>("Goal");
@@ -47,10 +49,85 @@ public partial class AsteroidField : Node3D
 
     }
 
+    public override void _Input(InputEvent @event)
+    {
+		if (@event is InputEventKey)
+		{
+			InputEventKey mover = (InputEventKey)@event;
+			if (mover.Keycode == Key.Left && mover.IsPressed())
+			{
+				leftThrust = true;
+			}
+			else if (mover.Keycode == Key.Left && mover.IsReleased())
+                {
+                    leftThrust = false;
+                }
+
+            if (mover.Keycode == Key.Right && mover.IsPressed())
+			{
+				rightThrust = true;
+			}
+			else if (mover.Keycode == Key.Right && mover.IsReleased())
+			{
+				rightThrust = false;
+			}
+
+
+
+			
+            if (mover.Keycode == Key.Space && mover.IsPressed())
+            {
+                thrust1 = 1;
+            }
+            else if (mover.Keycode == Key.Space && mover.IsReleased())
+            {
+				thrust1 = 0;
+            }
+
+            if (mover.Keycode == Key.B && mover.IsPressed())
+            {
+                thrust2 = 1;
+            }
+            else if (mover.Keycode == Key.B && mover.IsReleased())
+            {
+                thrust2 = 0;
+            }
+
+			forwardThrust = thrust1 + thrust2;
+			GD.Print(forwardThrust);
+        }
+    }
+
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		GlobalPosition += shipVelocity * (float)delta;
+		float zVel = forwardThrust;
+		
+
+		float xVel = 0;
+		if (rightThrust)
+		{
+			xVel++;
+		}
+		else
+		{
+			xVel--;
+		}
+
+        if (!leftThrust)
+        {
+            xVel++;
+        }
+        else
+        {
+            xVel--;
+        }
+
+		shipVelocity = new Vector3(-xVel * shipSpeed, 0, zVel * shipSpeed);
+
+		//GD.Print(shipVelocity.ToString());
+        GlobalPosition += shipVelocity * (float)delta;
+		GlobalPosition = new Vector3(Mathf.Clamp(GlobalPosition.X, -20, 20), 0, GlobalPosition.Z);
 	}
 
 	
