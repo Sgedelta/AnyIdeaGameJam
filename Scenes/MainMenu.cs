79f16ff2656a24bc527ea9c377b51db6b3ed961a
clc6900 <clc6900@MSS2150-24>
2026-03-22 16:32:47 -0400

    UI stuff

--- Scenes/MainMenu.cs
+++ Scenes/MainMenu.cs
@@ -3,18 +3,36 @@ using System;
 
 public partial class MainMenu : Control
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+    [Export]
+    private Button startButton;
+    [Export]
+    private Button howToButton;
+    [Export]
+    private Button quitButton;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        startButton.Pressed += LoadGameplay;
+        howToButton.Pressed += HowToPlay;
+        quitButton.Pressed += Quit;
+    }
 
-	public void LoadGameplay()
-	{
-		GetTree().ChangeSceneToFile("res://Scenes/TestWorld.tscn");
-	}
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+    }
+
+    public void LoadGameplay()
+    {
+        GetTree().ChangeSceneToFile("res://Scenes/AssetPopWorld.tscn");
+    }
+    public void HowToPlay()
+    {
+        GetTree().ChangeSceneToFile("res://Scenes/HowToScene.tscn");
+    }
+    public void Quit()
+    {
+        GetTree().Quit();
+    }
 }
